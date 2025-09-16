73ac518044847db821631210f19ec0a1eb600373
Rizkyalfasha <adityakrui518@gmail.com>
2025-09-16 15:27:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Contoh fungsi klik menu
-document.querySelectorAll(".menu-item").forEach(item => {
-  item.addEventListener("click", () => {
-    alert("Kamu memilih: " + item.innerText);
-  });
-});
